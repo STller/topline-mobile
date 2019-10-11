4b2be07ceb6f1053e8c11bead8b805ebe34b0fd4
温翔宇 <fishfly516@gmail.com>
2019-10-11 16:55:40 +0800

    axios封装

---
+++ src/utils/request.js
@@ -0,0 +1,11 @@
+// axios相关请求在这里
+import axios from 'axios'
+
+// 复制了一个axios 因为有时需要单独定制不同的请求
+// 可以设置多个 request 请求对象 每个对象拥有不同的配置
+const request = axios.create({
+  // 请求的基础路径
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+export default request
