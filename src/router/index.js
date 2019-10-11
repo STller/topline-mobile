7e4931d401323180c02f67d30ffbe132fef7fbd2
温翔宇 <fishfly516@gmail.com>
2019-10-11 17:08:37 +0800

    登陆路由挂载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/views/login/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+      // component: () => import('@/views/login/index.vue')
+    }
   ]
 })
