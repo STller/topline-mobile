55b7326d210d8a8522b64d16d38b1d1098b2b9ea
温翔宇 <fishfly516@gmail.com>
2019-10-11 17:02:00 +0800

    导入jsonBig解决大数问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 // axios相关请求在这里
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 // 复制了一个axios 因为有时需要单独定制不同的请求
 // 可以设置多个 request 请求对象 每个对象拥有不同的配置
@@ -7,5 +8,10 @@ const request = axios.create({
   // 请求的基础路径
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+
+// 解决大数问题
+request.defaults.transformResponse = [function (data) {
+  return data ? jsonBig.parse(data) : ''
+}]
 
 export default request
